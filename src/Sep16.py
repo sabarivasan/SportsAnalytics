a1f591519b443c390bb0083bce60b94ebd79f3ab
sabarivasan <sabarivasan@gmail.com>
2017-09-15 23:23:32 -0400

    Milestone 3 - Add wickets and type inference

--- src/Sep16.py
+++ src/Sep16.py
@@ -1,25 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-bowler = "Shane Warne"
+bowler = { "name": "Shane Warne" , "country" : "Australia"}
 
-ball_by_ball = [0, 1, 4, 0, 6, 0, 0, 1, 2, 6, 0, 0, 2, 1, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 3, 1, 0, 0,
-           2, 1, 3, 0, 6, 0, 2, 0, 0, 6, 0, 0, 1, 3, 1, 2, 4, 6, 0, 0, 2, 0, 0, 0, 4, 1, 6, 1, 2, 1]
+ball_by_ball = [0, 1, 4, 0, 6, 0, 0, 1, "W", 6, 0, 0, 2, 1, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 3, 1, 0, 0,
+           2, 1, 3, 0, 6, 0, 2, 0, 0, 6, 0, 0, "W", 3, 1, 2, 4, "W", 0, 0, 2, 0, 0, 0, 4, 1, 6, 1, 2, 1]
 
 sum = float(0)
+num_wickets = 0
 for r in ball_by_ball:
-    sum += r
+    if type(r) == int:
+        sum += r
+    elif r == "W":
+        num_wickets += 1
 
 econ_rate = sum/len(ball_by_ball) * 6
 
-print "{}: economy rate (runs per over) = {}".format(bowler, econ_rate)
+print "{}: economy rate (runs per over) = {}".format(bowler["name"], econ_rate)
+print "{}: num wickets = {}".format(bowler["name"], num_wickets)
 
-h = np.histogram(ball_by_ball, bins=6)
+ball_by_ball_cleansed = [r if type(r) == int else 0 for r in ball_by_ball]
 
-plt.hist(ball_by_ball, bins=6)
+
+h = np.histogram(ball_by_ball_cleansed, bins=6)
+
+plt.hist(ball_by_ball_cleansed, bins=6)
 plt.xlabel("Runs conceded")
 plt.ylabel("Number of occurrences")
-plt.title("Histogram of runs conceded by " + bowler)
+plt.title("Histogram of runs conceded by " + bowler["name"])
 plt.show()
 
 
